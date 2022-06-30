7ad1de646ea3d92cd3b0e74655c6a27ba13c109c
wtlin1228 <wtlin1228@gmail.com>
2022-06-30 23:24:38 +0800

    complete lab6 - generate a route lib

--- apps/store/src/app/app.tsx
+++ apps/store/src/app/app.tsx
@@ -10,6 +10,9 @@ import Typography from '@material-ui/core/Typography';
 import { Header } from '@bg-hoard/store/ui-shared';
 import { formatRating } from '@bg-hoard/store/util-formatters';
 
+import { Route, Routes, Link } from 'react-router-dom';
+import { StoreFeatureGameDetail } from '@bg-hoard/store/feature-game-detail';
+
 export const App = () => {
   return (
     <>
@@ -17,38 +20,44 @@ export const App = () => {
       <div className={styles['container']}>
         <div className={styles['games-layout']}>
           {getAllGames().map((x) => (
-            <Card key={x.id} className={styles['game-card']}>
-              <CardActionArea>
-                <CardMedia
-                  className={styles['game-card-media']}
-                  image={x.image}
-                  title={x.name}
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    {x.name}
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                    {x.description}
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                    className={styles['game-rating']}
-                  >
-                    <strong>Rating:</strong> {formatRating(x.rating)}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
+            <Link to={`/game/${x.id}`} key={x.id}>
+              <Card key={x.id} className={styles['game-card']}>
+                <CardActionArea>
+                  <CardMedia
+                    className={styles['game-card-media']}
+                    image={x.image}
+                    title={x.name}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {x.name}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="p"
+                    >
+                      {x.description}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="p"
+                      className={styles['game-rating']}
+                    >
+                      <strong>Rating:</strong> {formatRating(x.rating)}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Link>
           ))}
         </div>
       </div>
+
+      <Routes>
+        <Route path="/game/:id" element={<StoreFeatureGameDetail />} />;
+      </Routes>
     </>
   );
 };
