f04ac01ac3aa46232ea038d66517c3ee33163a27
wtlin1228 <wtlin1228@gmail.com>
2022-07-02 17:41:10 +0800

    complete lab11: e2e test the shared component

--- apps/store-ui-shared-e2e/src/integration/header/header.spec.ts
+++ apps/store-ui-shared-e2e/src/integration/header/header.spec.ts
@@ -1,7 +1,9 @@
 describe('store-ui-shared: Header component', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=header--primary'));
-    
-    it('should render the component', () => {
-      cy.get('h1').should('contain', 'Welcome to Header!');
-    });
+  beforeEach(() =>
+    cy.visit('/iframe.html?id=header--primary&args=title:BoardGameHoard')
+  );
+
+  it('should render the component', () => {
+    cy.get('h6').should('contain', 'BoardGameHoard');
+  });
 });
--- apps/store-ui-shared-e2e/src/integration/store-ui-shared/store-ui-shared.spec.ts
+++ apps/store-ui-shared-e2e/src/integration/store-ui-shared/store-ui-shared.spec.ts
@@ -1,7 +1,7 @@
 describe('store-ui-shared: StoreUiShared component', () => {
   beforeEach(() => cy.visit('/iframe.html?id=storeuishared--primary'));
-    
-    it('should render the component', () => {
-      cy.get('h1').should('contain', 'Welcome to StoreUiShared!');
-    });
+
+  it('should render the component', () => {
+    cy.get('h1').should('contain', 'Welcome to StoreUiShared!');
+  });
 });
