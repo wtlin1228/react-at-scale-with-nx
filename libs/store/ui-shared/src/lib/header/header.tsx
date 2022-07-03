f898529c35ee7bc1e362d137cc4275996dc8ce04
Leo <wtlin1228@gmail.com>
2022-07-04 03:01:25 +0800

    complete lab17 - nx cloud github bot (#1)
    * complete lab17 - nx cloud github bot
    
    * ci: use node 16
    
    * upgrade material-ui to v5
    
    * ci: use actions/cache directlly
    
    * ci: setup npm cache dir
    
    * ci: use npm install
    
    * ci: base on main
    
    * ci: base on main
    
    * ci: base on remotes/origin/main
    
    * ci: fetch refs
    
    * ci: install webpack5 for storybook

--- libs/store/ui-shared/src/lib/header/header.tsx
+++ libs/store/ui-shared/src/lib/header/header.tsx
@@ -1,33 +1,23 @@
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import { styled } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
+const StyledTypography = styled(Typography)({
+  '&': {
     flexGrow: 1,
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+});
 
 export interface HeaderProps {
   title: string;
 }
 
 export const Header = ({ title }: HeaderProps) => {
-  const classes = useStyles();
-
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6" className={classes.title}>
-          {title}
-        </Typography>
+        <StyledTypography variant="h6">{title}</StyledTypography>
       </Toolbar>
     </AppBar>
   );
