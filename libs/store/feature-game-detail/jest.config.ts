84bf4da1dbd94c085e67297c85b8f164f68fb72c
wtlin1228 <wtlin1228@gmail.com>
2022-09-30 15:22:16 +0800

    migrate nx to 14.8.2

--- libs/store/feature-game-detail/jest.config.ts
+++ libs/store/feature-game-detail/jest.config.ts
@@ -3,7 +3,7 @@ export default {
   displayName: 'store-feature-game-detail',
   preset: '../../../jest.preset.js',
   transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../../coverage/libs/store/feature-game-detail',
